da96a6c9a0324674f54e4cf2a77d5554c18dfea0
Emma Cardon <emma.cardon7@gmail.com>
2021-06-01 14:36:44 +0100

    created the cake create and new actions and view

--- app/controllers/cakes_controller.rb
+++ app/controllers/cakes_controller.rb
@@ -7,9 +7,13 @@ class CakesController < ApplicationController
   end
 
   def new
+    @cake = Cake.new
   end
 
   def create
+    @cake = Cake.new(cake_params)
+    @cake.save
+    redirect_to cake_path(@cake)
   end
 
   def edit
@@ -19,5 +23,11 @@ class CakesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def cake_params
+  params.require(:cake).permit(:name, :price, :description, :size)
   end
 end
