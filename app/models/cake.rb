4415a25dee0c818a9f1e1772060e3b61a9df4957
Emma Cardon <emma.cardon7@gmail.com>
2021-06-01 11:14:29 +0100

    added asssociations and validations to the cake model

--- app/models/cake.rb
+++ app/models/cake.rb
@@ -1,3 +1,9 @@
 class Cake < ApplicationRecord
   belongs_to :user
+  has_many :orders
+  has_many :users, through: :order
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :description, presence: true
+  validates :size, presence: true
 end
