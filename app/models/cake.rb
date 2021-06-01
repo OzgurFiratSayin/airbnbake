c025216ec43443f463d284527bb51f2345fd4118
Natalia Rossini <naty.rossini@hotmail.com>
2021-06-01 12:05:19 +0100

    added empty methods

--- app/models/cake.rb
+++ app/models/cake.rb
@@ -1,7 +1,7 @@
 class Cake < ApplicationRecord
   belongs_to :user
   has_many :orders
-  has_many :users, through: :order
+  # has_many :users, through: :order
   validates :name, presence: true
   validates :price, presence: true
   validates :description, presence: true
