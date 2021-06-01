829ffdd65b4ef242d420ebe134c0f2ebd7e20800
Ozgur Firat Sayin <ozgurfiratsayin@gmail.com>
2021-06-01 14:36:32 +0100

    Edit and update methods in controller and edit page in view

--- app/controllers/cakes_controller.rb
+++ app/controllers/cakes_controller.rb
@@ -13,11 +13,21 @@ class CakesController < ApplicationController
   end
 
   def edit
+    @flat = Flat.find(params[:id])
   end
 
   def update
+    @cake = Cake.find(params[:id])
+    @cake.update(cake_params)
+    redirect_to cake_path(@cake)
   end
 
   def destroy
+  end
+
+  private
+
+  def cake_params
+    params.require(:cake).permit(:name, :price, :description, :size)
   end
 end
