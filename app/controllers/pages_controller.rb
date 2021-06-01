a3af1cab87668298e2913ca85b0446a5ce5e3ca9
Natalia Rossini <naty.rossini@hotmail.com>
2021-06-01 23:54:41 +0100

    tried to make search feature and add some style

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,15 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: [:home, :search]
   def home
+  end
+
+  def search
+    @cakes = Cake.all
+    if params[:search].blank?
+      render :home
+    else
+      # @parameter = params[:search]
+      @results = Cake.where("name ILIKE ?", params[:search])
+    end
   end
 end
