51de50d31354776cba413674fb9a9702ce40f30d
Natalia Rossini <naty.rossini@hotmail.com>
2021-06-01 15:58:58 +0100

    created orders routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :cakes
+  # get '/dashboard', to: users#show, as: :dashboard #to show all users cakes/orders
+  resources :cakes do
+    resources :orders, only: [:new, :create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
