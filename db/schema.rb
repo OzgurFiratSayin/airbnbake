5f73be8e0f51406d5938c2f3c4bce0cab118f8a3
Emma Cardon <emma.cardon7@gmail.com>
2021-06-03 15:28:11 +0100

    added latitude and longitude columns to user table and added geocoding settings to the User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_03_113130) do
+ActiveRecord::Schema.define(version: 2021_06_03_135513) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,6 +88,8 @@ ActiveRecord::Schema.define(version: 2021_06_03_113130) do
     t.string "first_name"
     t.string "last_name"
     t.text "address"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
